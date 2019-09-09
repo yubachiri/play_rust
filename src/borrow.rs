ecb019f31280df9336e9bb220b908f63a44b3339
yubachiri <k.i.moso9532@gmail.com>
2019-09-09 10:07:34 +0900

    ちょっと表記変更

--- src/borrow.rs
+++ src/borrow.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let can_ref = 5;
-    use_copy(can_ref);
-    println!("can_ref is {}", can_ref);
+    let can_copy = 5;
+    use_copy(can_copy);
+    println!("can_ref is {}", can_copy);
 
     let cannot_ref: Vec<i32> = vec![5; 5];
     use_ref(cannot_ref);
-    // println!("cannot_ref is {}", cannot_ref[0]);
+    // println!("cannot_ref is {}", cannot_ref[0]); // これは所有権がないので読めない
 }
 
 fn use_copy(num: i32) {
