7f399f10942fe90410789ef80dd9596aa4c92c8b
yubachiri <k.i.moso9532@gmail.com>
2019-09-11 23:19:41 +0900

    トレイトオブジェクトの章学んだけどよくわかってない

---
+++ src/trait.rs
@@ -0,0 +1,30 @@
+trait Foo {
+    fn method(&self) -> String;
+}
+
+impl Foo for u8 {
+    fn method(&self) -> String { format!("u8: {}", *self) }
+}
+
+impl Foo for String {
+    fn method(&self) -> String { format!("string!: {}", *self) }
+}
+
+fn do_something<T: Foo>(x: T) {
+    x.method();
+}
+
+fn do_another_something(x: &Foo) {
+    x.method();
+}
+
+fn main() {
+    let x = 5u8;
+    let y = "Hello".to_string();
+
+    do_something(x);
+    do_something(y);
+
+    let z = 5u8;
+    do_another_something(&z as &Foo);
+}
