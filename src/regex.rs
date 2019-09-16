34826834800f7236262a5937c04b23982df23ebf
yubachiri <k.i.moso9532@gmail.com>
2019-09-16 17:46:50 +0900

    正規表現修正

--- src/regex.rs
+++ src/regex.rs
@@ -5,7 +5,9 @@ fn main() {
     let comment_pattern = r"//.*";
     let num_pattern = r"(?P<num>[0-9]+)";
     let id_pattern = r"(?P<id>[A-Z_a-z][A-Z_a-z0-9]*|==|<=|>=|&&|\|\||\p{Punct})";
-    let str_pattern = "\"(\\n|[^\"])*";
+    // 文字列リテラルの条件
+    // 「"」から始まり、expressionがあって、「"」で終わるもの
+    let str_pattern = r#"(\\"|\\|\n|[^"])*"#;
 
     let set = RegexSet::new(&[
         emp_pattern, // 0
@@ -16,16 +18,16 @@ fn main() {
     ]).unwrap();
 
     let target_num = 12345.to_string();
-    let target_str = "sample";
+    let target_str = r#"sample"#;
 
     let num_matches: Vec<_> = set.matches(&target_num).into_iter().collect();
-    assert_eq!(num_matches, vec![0, 2]);
+    assert_eq!(num_matches, vec![0, 2, 3]);
     let re1 = Regex::new(num_pattern).unwrap();
     let cap_num = re1.captures(&target_num).unwrap();
     assert_eq!("12345", &cap_num["num"]);
 
     let str_matches: Vec<_> = set.matches(&target_str).into_iter().collect();
-    assert_eq!(str_matches, vec![0, 4]);
+    assert_eq!(str_matches, vec![0, 3, 4]);
     let re2 = Regex::new(str_pattern).unwrap();
     let cap_str = re2.captures(&target_str).unwrap();
     assert_eq!("sample", &cap_str[0]);
