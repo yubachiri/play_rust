e6f09398fea2836f6c960ee8a2f507a583042033
yubachiri <k.i.moso9532@gmail.com>
2019-09-29 11:21:20 +0900

    コピペした

--- src/calculator.rs
+++ src/calculator.rs
@@ -1,83 +1,39 @@
 extern crate combine;
 extern crate combine_language;
 
-use combine::char::{string, spaces, letter, alpha_num};
-use combine::{Parser, satisfy, Stream, ParseResult, ParseError, many1};
-use combine_language::{LanguageEnv, LanguageDef, Identifier};
+use combine::char::{string, letter, spaces};
+use combine::{Parser, many};
+use combine_language::{Assoc, Fixity, expression_parser};
+//TODO unignore once its stops ICEing
+use self::Expr::*;
 
-// #[derive(Debug, Clone, PartialEq, Eq)]
-// enum Expr {
-//     Number(i64),
-//     Plus(Box<Expr>, Box<Expr>),
-//     Minus(Box<Expr>, Box<Expr>),
-//     Times(Box<Expr>, Box<Expr>),
-//     Divides(Box<Expr>, Box<Expr>),
-// }
-
-fn calc_env<'a, I: 'a>() -> LanguageEnv<'a, I> 
-    where
-        I: Stream<Item = char>,
-        I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    LanguageEnv::new(LanguageDef {
-        ident: Identifier {
-            start: letter(),
-            rest: alpha_num(),
-            reserved: ["if", "then", "else", "let", "in", "type"].iter()
-                                                                .map(|x| (*x).into())
-                                                                .collect(),
-        },
-        op: Identifier {
-            start: satisfy(|c| "+-*/".chars().any(|x| x == c)),
-            rest: satisfy(|c| "+-*/".chars().any(|x| x == c)),
-            reserved: ["+", "-", "*", "/"].iter().map(|x| (*x).into()).collect()
-        },
-        comment_start: string("/*").map(|_| ()),
-        comment_end: string("*/").map(|_| ()),
-        comment_line: string("//").map(|_| ()),
-    })
+#[derive(PartialEq, Debug)]
+enum Expr {
+    Id(String),
+    Op(Box<Expr>, &'static str, Box<Expr>)
+}
+fn op(l: Expr, o: &'static str, r: Expr) -> Expr {
+    Op(Box::new(l), o, Box::new(r))
+}
+fn id(s: &str) -> Expr {
+    Id(String::from(s))
 }
 
-// fn numbers<I>(input: I) -> ParseResult<Box<Expr>, I>
-//     where
-//         I: Stream<Item=char>,
-//         I: combine::StreamOnce,
-//         <I as combine::StreamOnce>::Error: combine::ParseError<char, <I as combine::StreamOnce>::Range, <I as combine::StreamOnce>::Position>
-// {
-//     let env = calc_env();
-//     let number = env.integer().map(|x| Box::new(Expr::Number(x)));
-//     // let parenthesized = env.parens(parser(expr));
-//     number.parse_stream(input)
-// }
-
-// fn integer_literal<I: 'static + Stream<Item=char>>() -> Box<Parser<Input = I, Output = i64>> {
-//     let sign = optional(token('+').or(token('-')));
-
-//     let number = many1::<Vec<char>, _>(digit())
-//         .map(|ds| {
-//             ds.into_iter()
-//                 .map(|c| c.to_digit(10).unwrap() as i64)
-//                 .fold(0, |acc, x| acc * 10 + x)
-//         });
-
-//     let signed_number = (sign, number)
-//         .map(|(s, num)| {
-//             match s {
-//                 None | Some('+') => num,
-//                 Some('-') => -num,
-//                 _ => unreachable!(),
-//             }
-//         });
-
-//     Box::new(signed_number)
-// }
-
-fn main() {
-    let env = calc_env();
-    // let id = env.identifier();//An identifier parser
-    let mut integer = many(env.integer());//An integer parser
-    // let result = (id, integer).easy_parse("this /* Skips comments */ 42");
-    let result = integer.easy_parse("42 55 12");
-    assert_eq!(result, Ok((42, "")));
-    // assert_eq!(result, Ok(((String::from("this"), 42), "")));
+fn main(){
+    let op_parser = string("+").or(string("*"))
+        .map(|op| {
+            let prec = match op {
+                "+" => 6,
+                "*" => 7,
+                _ => unreachable!()
+            };
+            (op, Assoc { precedence: prec, fixity: Fixity::Left })
+        })
+        .skip(spaces());
+    let term = many(letter())
+        .map(Id)
+        .skip(spaces());
+    let mut parser = expression_parser(term, op_parser, op);
+    let result = parser.parse("a + b * c + d");
+    assert_eq!(result, Ok((op(op(id("a"), "+", op(id("b"), "*", id("c"))), "+", id("d")), "")));
 }
