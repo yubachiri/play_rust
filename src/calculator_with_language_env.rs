e2158a79fb88e52350a6b7b89c3b25a81715480c
yubachiri <k.i.moso9532@gmail.com>
2019-10-15 19:01:31 +0900

    途中のやつとかも全部諸々一旦投げちゃう

--- src/calculator_with_language_env.rs
+++ src/calculator_with_language_env.rs
@@ -1,9 +1,9 @@
 extern crate combine;
 extern crate combine_language;
 
+use combine::*;
+use combine_language::*;
 use combine::char::{string, letter, alpha_num};
-use combine::{Parser, satisfy, Stream, ParseResult, ParseError, parser, chainl1};
-use combine_language::{LanguageEnv, LanguageDef, Identifier};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum Expr {
@@ -14,26 +14,53 @@ enum Expr {
     Divides(Box<Expr>, Box<Expr>),
 }
 
-fn calc_env<'a, I: 'a>() -> LanguageEnv<'a, I> 
-    where
-        I: Stream<Item = char>,
-        I::Error: ParseError<I::Item, I::Range, I::Position>,
+// fn calc_env<'a, I: 'a>() -> LanguageEnv<'a, I> 
+//     where
+//         I: Stream<Item = char>,
+//         I::Error: ParseError<I::Item, I::Range, I::Position>,
+// {
+//     LanguageEnv::new(LanguageDef {
+//         ident: Identifier {
+//             start: letter(),
+//             rest: alpha_num(),
+//             reserved: ["if", "then", "else", "let", "in", "type"].iter()
+//                                                                 .map(|x| (*x).into())
+//                                                                 .collect(),
+//         },
+//         op: Identifier {
+//             start: satisfy(|c| "+-*/".chars().any(|x| x == c)),
+//             rest: satisfy(|c| "+-*/".chars().any(|x| x == c)),
+//             reserved: ["+", "-", "*", "/"].iter().map(|x| (*x).into()).collect()
+//         },
+//         comment_start: string("/*").map(|_| ()),
+//         comment_end: string("*/").map(|_| ()),
+//         comment_line: string("//").map(|_| ()),
+//     })
+// }
+
+fn language_env<'a, I: 'a>() -> LanguageEnv<'a, I>
+    where I: Stream<Item=char>
 {
     LanguageEnv::new(LanguageDef {
-        ident: Identifier {
-            start: letter(),
-            rest: alpha_num(),
-            reserved: ["if", "then", "else", "let", "in", "type"].iter()
-                                                                .map(|x| (*x).into())
-                                                                .collect(),
-        },
-        op: Identifier {
-            start: satisfy(|c| "+-*/".chars().any(|x| x == c)),
-            rest: satisfy(|c| "+-*/".chars().any(|x| x == c)),
-            reserved: ["+", "-", "*", "/"].iter().map(|x| (*x).into()).collect()
-        },
-        comment_start: string("/*").map(|_| ()),
-        comment_end: string("*/").map(|_| ()),
-        comment_line: string("//").map(|_| ()),
+    ident: Identifier {
+        start: letter(),
+        rest: alpha_num(),
+        reserved: ["if", "then", "else", "let", "in", "type"].iter()
+                                                             .map(|x| (*x).into())
+                                                             .collect(),
+    },
+    op: Identifier {
+        start: satisfy(|c| "+-*/".chars().any(|x| x == c)),
+        rest: satisfy(|c| "+-*/".chars().any(|x| x == c)),
+        reserved: ["+", "-", "*", "/"].iter().map(|x| (*x).into()).collect()
+    },
+    comment_start: string("/*").map(|_| ()),
+    comment_end: string("*/").map(|_| ()),
+    comment_line: string("//").map(|_| ()),
     })
+}
+
+fn number() {
+    let env = language_env();
+    let number = enb.integer();
 }
